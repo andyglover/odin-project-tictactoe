2b36aa5c7ad3b0deaeab32c2bdaf3cb08d13194b
Andy Glover <andrew.l.glover@gmail.com>
2022-09-05 22:25:12 -0700

    Make gameboard object

--- src/script.js
+++ src/script.js
@@ -4,7 +4,12 @@ const playerFactory = (name, symbol) => {
     const sayHello = () => console.log(`hello, I'm ${name} and my symbol is ${symbol}.`);
     return { name, symbol, sayHello };
 }
-var gameboard = [];
+
+const gameboard = (() => {
+    return {
+        
+    };
+})();
 const playerOne = playerFactory("P1", "X")
 const playerTwo = playerFactory("P2", "O")
 playerOne.sayHello();

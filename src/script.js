ae26fc4823aad9c42498ab0c575d8a08516cc35b
Andy Glover <andrew.l.glover@gmail.com>
2022-09-05 22:57:50 -0700

    Add gameboard render

--- src/script.js
+++ src/script.js
@@ -6,11 +6,31 @@ const playerFactory = (name, symbol) => {
 }
 
 const gameboard = (() => {
-    return {
+    const spaces = [
+        "x",
+        "o",
+        "x",
+        "o",
+        "x",
+        "o",
+        "x",
+        "o",
+        "x"
+    ];
+    const render = function () {
+        spaces.forEach(element => {
+            const div = document.createElement('div');
+            div.classList.add('grid-item');
+            div.textContent=element;
+            const container = document.querySelector("#grid-container");
+            container.appendChild(div);
+        });
         
-    };
+    }
+    return { render };
 })();
 const playerOne = playerFactory("P1", "X")
 const playerTwo = playerFactory("P2", "O")
 playerOne.sayHello();
 playerTwo.sayHello();
+gameboard.render();
